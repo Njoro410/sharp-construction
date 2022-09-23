87a59212b0548eef76b3cc425c931a537c604b06
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-23 11:17:15 +0300

    updated css on images and cards

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,67 +2,83 @@ import React from "react";
 
 const Services = () => {
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto">
       <div className="flex justify-center items-center">
-        <h3 className="px-5">Our Services</h3>
+        <h3 className="py-5 text-5xl">Our Services</h3>
       </div>
       <div className="grid md:grid-cols-2 px-8 mt-12">
         <div className="grid md:grid-rows-2 mx-4 ">
           <div className="flex justify-center items-center">
-            <div className="card w-96 glass">
+            <div className="card w-[30rem] bg-base-100 my-5 shadow-xl">
               <figure>
-                <img src="https://placeimg.com/400/225/arch" alt="car!" />
+                <img
+                  className="w-[100%] object-cover h-[15vw]"
+                  src="https://res.cloudinary.com/hamstech/images/v1631518640/Hamstech%20App/Top-Interior-Design-Trends-to-Design-Your-Dream-Home_804646102d455/Top-Interior-Design-Trends-to-Design-Your-Dream-Home_804646102d455.jpg?_i=AA"
+                  alt="car!"
+                />
               </figure>
               <div className="card-body">
-                <h2 className="card-title">Life hack</h2>
+                <h2 className="card-title">Interior Design</h2>
                 <p>How to park your car at your garage?</p>
-                <div className="card-actions justify-end">
+                {/* <div className="card-actions justify-end">
                   <button className="btn btn-primary">Learn now!</button>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
           <div className="flex justify-center items-center">
-            <div className="card w-96 glass">
+            <div className="card w-[30rem] bg-base-100 my-5 shadow-xl">
               <figure>
-                <img src="https://placeimg.com/400/225/arch" alt="car!" />
+                <img
+                  className="w-[100%] object-cover h-[15vw]"
+                  src="https://3.bp.blogspot.com/-qLhr4SO7N3I/XC33cSn4N8I/AAAAAAAAigY/hVJgrijJh5gFtjgblHxsghIX5Cz3fGxAgCEwYBhgL/s1600/Latest-gypsum-ceiling-designs-and-ideas-2019%2B%25287%2529.jpg"
+                  alt="car!"
+                />
               </figure>
               <div className="card-body">
-                <h2 className="card-title">Life hack</h2>
+                <h2 className="card-title">Gypsum Designs</h2>
                 <p>How to park your car at your garage?</p>
-                <div className="card-actions justify-end">
+                {/* <div className="card-actions justify-end">
                   <button className="btn btn-primary">Learn now!</button>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
         </div>
         <div className="grid md:grid-rows-2">
           <div className="flex justify-center items-center">
-            <div className="card w-96 glass">
+            <div className="card w-[30rem] bg-base-100 my-5 shadow-xl">
               <figure>
-                <img src="https://placeimg.com/400/225/arch" alt="car!" />
+                <img
+                  className="w-[100%] object-cover h-[15vw]"
+                  src="http://cdn.floorcoveringsinternational.com/wp-content/uploads/sites/1053/2017/07/09174524/shutterstock_45661648.jpg"
+                  alt="car!"
+                />
               </figure>
               <div className="card-body">
-                <h2 className="card-title">Life hack</h2>
+                <h2 className="card-title">Wood and Tile Works</h2>
                 <p>How to park your car at your garage?</p>
-                <div className="card-actions justify-end">
+                {/* <div className="card-actions justify-end">
                   <button className="btn btn-primary">Learn now!</button>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
           <div className="flex justify-center items-center">
-            <div className="card w-96 glass">
+            <div className="card w-[30rem] bg-base-100 my-5 shadow-xl">
               <figure>
-                <img src="https://placeimg.com/400/225/arch" alt="car!" />
+                <img
+                  className="w-[100%] object-cover h-[15vw]"
+                  src="https://www.rexeroofing.com/rexeloads/uploads/2019/05/two-tone-stone-coated-residential-Garden-Estate.jpg"
+                  alt="car!"
+                />
               </figure>
               <div className="card-body">
-                <h2 className="card-title">Life hack</h2>
+                <h2 className="card-title">Roofing</h2>
                 <p>How to park your car at your garage?</p>
-                <div className="card-actions justify-end">
+                {/* <div className="card-actions justify-end">
                   <button className="btn btn-primary">Learn now!</button>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
