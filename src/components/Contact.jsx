c60bee1e7f01b14df40045e3ab26cafa8a0e0934
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-24 16:00:29 +0300

    added contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,110 @@
-import React from 'react'
-
 const Contact = () => {
   return (
-    <div className='w-screen z-0  '>
-        <div className='grid grid-cols-3'>
-            <div>
-                logo
+    <div className="w-full bg-slate-800">
+      <div className="flex justify-center items-center w-full text-3xl font-bold leading-tight">
+        <h1 className="text-white py-5">Contact Us</h1>
+      </div>
+      <div className="grid md:grid-cols-2 md:divide-x">
+        <div className="flex justify-center text-white items-center">
+          <div className="py-6 md:py-0 md:px-6">
+            <h1 className="text-4xl font-bold">Get in touch</h1>
+            <p className="pt-2 pb-4">
+              Fill in the form to start a conversation
+            </p>
+            <div className="space-y-4">
+              <p className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5 mr-2 sm:mr-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Fake address, 9999 City</span>
+              </p>
+              <p className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5 mr-2 sm:mr-6"
+                >
+                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+                </svg>
+                <span>123456789</span>
+              </p>
+              <p className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5 mr-2 sm:mr-6"
+                >
+                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                </svg>
+                <span>sharp@business.com</span>
+              </p>
             </div>
-            <div>
-                customer support
-            </div>
-
+          </div>
         </div>
+        <div>
+          <section className="p-6 text-slate-100">
+            <form className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow bg-slate-800">
+              <div>
+                <label for="name" className="block mb-1 ml-1">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  placeholder="Your name"
+                  required=""
+                  className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-400 bg-slate-600"
+                />
+              </div>
+              <div>
+                <label for="email" className="block mb-1 ml-1">
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="Your email"
+                  required=""
+                  className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-400 bg-slate-600"
+                />
+              </div>
+              <div>
+                <label for="message" className="block mb-1 ml-1">
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  type="text"
+                  placeholder="Message..."
+                  className="block w-full p-2 rounded autoexpand focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-400 bg-slate-600"
+                ></textarea>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full px-4 py-2 font-bold rounded shadow focus:outline-none focus:ring hover:ring focus:ring-opacity-50 bg-violet-400 focus:ring-violet-400 hover:ring-violet-400 text-slate-900"
+                >
+                  Send
+                </button>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
