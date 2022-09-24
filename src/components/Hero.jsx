a7455129e080847dac9ff083c451a981cfabd6b0
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-24 15:59:24 +0300

    added more style for user appeal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,6 +10,7 @@ const Hero = () => {
           <div className="h-[25rem]">
             <img
               className="h-[25rem]"
+              alt="img"
               src="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW50ZXJpb3IlMjBkZXNpZ258ZW58MHx8MHx8&w=1000&q=80"
             />
             <p className="legend">Legend 1</p>
@@ -17,6 +18,7 @@ const Hero = () => {
           <div className="h-[25rem]">
             <img
               className="h-[25rem]"
+              alt="img"
               src="https://img.freepik.com/premium-photo/tropical-interior-design-living-room_269031-60.jpg?w=2000"
             />
             <p className="legend">Legend 2</p>
@@ -24,19 +26,20 @@ const Hero = () => {
           <div className="h-[25rem]">
             <img
               className="h-[25rem]"
+              alt = "img"
               src="https://www.decorilla.com/online-decorating/wp-content/uploads/2018/10/modern-interior-design-grey-living-room2.png"
             />
             <p className="legend">Legend 3</p>
           </div>
         </Carousel>
       </div>
-      <div className="relative w-full h-[25rem] bg-gradient-to-r from-black grid md:grid-cols-2 ">
-        <div className="container mx-auto px-4">
-          <div className="relative top-[20%] p-4 md:p-8 text-white">
-            <h1 className="text-3xl md:text-5xl font-bold ">
-              SHARP CONSTRUCTION AND DESIGN COMPANY
+      <div className="relative w-full h-[25rem] bg-gradient-to-r from-black flex ">
+        <div className="container mx-auto ">
+          <div className="relative top-[20%] p-4  text-white">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold md:w-2/4">
+              SHARP <span className="text-yellow-400">CONSTRUCTION</span>  AND <span className="text-yellow-400">DESIGN</span>  COMPANY
             </h1>
-            <p className="w-full my-5 text-gray-200">
+            <p className="w-full my-5 text-gray-200 md:w-3/4">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
               nemo, voluptas, quo libero explicabo dolor totam quam possimus,
               ducimus sed asperiores eum ad molestias nisi et illo dicta
